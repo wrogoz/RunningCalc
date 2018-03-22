e91fa4913d228e67c93c16d38bf483386c746e14
wrogoz <wrogoz@gmail.com>
2018-03-22 23:03:19 +0100

    baaardzo wczesny zarys kalkulatora - projektowanie funkcji w JS - wartosci min/sek - bez godzin

---
+++ calc.js
@@ -0,0 +1,16 @@
+
+// dane do wprowadzenia czas / odległosc
+//czas sprowadzic do wspolnego mianownika
+
+function sugerowaneTempo ( szacowanyCzasWmin, km){
+// min/km jako int
+var szacowanyCzasWsek=szacowanyCzasWmin*60;
+ var min= Math.floor(Math.floor(szacowanyCzasWsek/km)/60);
+ //reszta sekund
+ var sek= (szacowanyCzasWsek/km)%60;
+ // zwróć: tempo
+
+ return min + ":" + sek.toFixed(0) ;
+}
+
+
