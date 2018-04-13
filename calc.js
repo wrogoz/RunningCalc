186cf0971a633276a1d7cbbf1bdaee1a3a654d3b
wrogoz <wrogoz@gmail.com>
2018-04-13 22:47:10 +0200

    dodano jQ

--- calc.js
+++ calc.js
@@ -1,17 +1,114 @@
+var godz=$("#godziny").val();
+var szacowanyCzasWmin=$("#minuty").val();
+var sekundy=$("#sekundy").val();
+var km=$("#km").val();
+
+
+$("#przelicz").on("click", function (){
+
+var wynik=function sugerowaneTempo (godz, szacowanyCzasWmin,sekundy, km){
+
+
+	
+
+	//				 min/km jako int
+	var szacowanyCzasWsek=szacowanyCzasWmin*60;
+	szacowanyCzasWsek+=sekundy;
+	szacowanyCzasWsek+=godz*3600;
+
+	 var min= Math.floor(Math.floor(szacowanyCzasWsek/km)/60);
+	 //				reszta sekund
+	 var sek= (szacowanyCzasWsek/km)%60;
+	 // 			zwróć: tempo
+		if (sek.toString().length === 1) {					
+	    	sek = '0' + sek;
+	    	return min + ":" + sek;
+		}
+	 	else{
+	 		return min + ":" + sek.toFixed(0) ;
+	 		}
+		
+}; 	
+
+
+$("#wynik").text(wynik);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//		------------------->funkcje<-------------------------
+
 
 // dane do wprowadzenia czas / odległosc
 //czas sprowadzic do wspolnego mianownika
 
-function sugerowaneTempo ( szacowanyCzasWmin, km){
-// min/km jako int
-var szacowanyCzasWsek=szacowanyCzasWmin*60;
- var min= Math.floor(Math.floor(szacowanyCzasWsek/km)/60);
- //reszta sekund
- var sek= (szacowanyCzasWsek/km)%60;
- // zwróć: tempo
 
- return min + ":" + sek.toFixed(0) ;
+/* 
+function sugerowaneTempo ( szacowanyCzasWmin, km){
+	//				 min/km jako int
+	var szacowanyCzasWsek=szacowanyCzasWmin*60;
+	 var min= Math.floor(Math.floor(szacowanyCzasWsek/km)/60);
+	 //				reszta sekund
+	 var sek= (szacowanyCzasWsek/km)%60;
+	 // 			zwróć: tempo
+		if (sek.toString().length === 1) {					
+	    	sek = '0' + sek;
+	    	return min + ":" + sek;
+		}
+	 	else{
+	 		return min + ":" + sek.toFixed(0) ;
+	 		}
 }
+
+function sugerowaneTempo ( szacowanyCzasWmin,sekundy, km){
+	//				 min/km jako int
+	var szacowanyCzasWsek=szacowanyCzasWmin*60;
+	szacowanyCzasWsek+=sekundy;
+	console.log(szacowanyCzasWsek);
+	 var min= Math.floor(Math.floor(szacowanyCzasWsek/km)/60);
+	 //				reszta sekund
+	 var sek= (szacowanyCzasWsek/km)%60;
+	 // 			zwróć: tempo
+		if (sek.toString().length === 1) {					
+	    	sek = '0' + sek;
+	    	return min + ":" + sek;
+		}
+	 	else{
+	 		return min + ":" + sek.toFixed(0) ;
+	 		}
+	}
+	function sugerowaneTempo (godz, szacowanyCzasWmin,sekundy, km){
+	//				 min/km jako int
+	var szacowanyCzasWsek=szacowanyCzasWmin*60;
+	szacowanyCzasWsek+=sekundy;
+	szacowanyCzasWsek+=godz*3600;
+	console.log(szacowanyCzasWsek);
+	 var min= Math.floor(Math.floor(szacowanyCzasWsek/km)/60);
+	 //				reszta sekund
+	 var sek= (szacowanyCzasWsek/km)%60;
+	 // 			zwróć: tempo
+		if (sek.toString().length === 1) {					
+	    	sek = '0' + sek;
+	    	return min + ":" + sek;
+		}
+	 	else{
+	 		return min + ":" + sek.toFixed(0) ;
+	 		}
+	} 		 		
+
+*/
+
 
 
 
